99178030ecba43ee9cc8ab031aa68f554520445c
Joshua Matammu <josh@madmarketinggroup.com>
2020-09-11 17:05:13 +1000

    Added new tag manager

--- src/navbar.php
+++ src/navbar.php
@@ -1,5 +1,5 @@
 <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TQZNR54"
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5SMP6ZH"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
 

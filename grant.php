0fe06746e5c62b5888732d4451461175d1df0385
Samuel Gander <sam.gander99@live.com>
2020-09-16 14:32:29 +1000

    updated btn

--- grant.php
+++ grant.php
@@ -31,7 +31,7 @@
               portfolio and what better way to do it than by helping local businesses in
               these difficult times.
                </p>
-              <a href="mailto:hi@madmarketinggroup.com.au"><button type="button" name="button" class="grant-btn btn-primary btn-mad btn-highlight btn-text-animate" btn-text="Apply Now"><span>Apply Now</span></button></a>
+              <a href="mailto:hi@madmarketinggroup.com.au"><button type="button" name="button" class="btn-primary btn-mad btn-highlight btn-text-animate" btn-text="Apply Now"><span>Apply Now</span></button></a>
             </div>
           </div>
           <div class="col-md-6 hero-header" style="z-index:5">

e272318be9d2406fb3815fc72595f4b619d3f50e
Goose <sam.gander99@live.com>
2021-07-14 09:13:47 +1000

    added title and changed favicon

--- src/meta.php
+++ src/meta.php
@@ -1,12 +1,13 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width, height=device-height" />
+  <title>Mad Marketing - Digital Marketing Agency</title>
   <meta name="facebook-domain-verification" content="pc7pjqf14c0331iwgwx262cckn7uzq" />
   <link rel="stylesheet" type="text/css" href="https://snipes-murphy.github.io/HydroUI/hydroui.css">
   <link rel="stylesheet" href="assets/owl.carousel.min.css">
   <link rel="stylesheet" href="assets/owl.theme.default.min.css">
   <link rel="stylesheet" type="text/css" href="css/style.css">
-  <link rel="icon" type="image/png" href="img/logo.png">
+  <link rel="icon" type="image/png" href="img/favicon.png">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100&display=swap" rel="stylesheet">
